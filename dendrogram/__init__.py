7bb9e5e329dbae712bac855c686d0089f30e0cf8
Bill Chen (Mac) <ybchen@umich.edu>
2021-07-16 23:40:50 +0800

    Updated __init__

---
+++ dendrogram/__init__.py
@@ -0,0 +1,7 @@
+from .percolation3D import latticeND
+from .structureTree3D import clusterTree3D, makeTree3D
+
+__all__ = ["percolation3D", "structureTree3D"]
+__version__ = "0.0.dev"
+__name__ = "dendrogram"
+__author__ = ["Bill Chen", "Xi Meng"]
