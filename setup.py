319176417923ec3fefbaa797eef94ebd74dc7f51
Bill Chen <ybchen@umich.edu>
2021-07-17 22:55:05 +0800

    Added setup: can be pip installed now!

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name = 'dendrogram',
+    packages = find_packages(),
+    version = '0.1.dev',
+    url = "https://github.com/EnthalpyBill/Dendrogram",
+    license = "MIT",
+    author = ["Xi Meng", "Bill Chen"],
+    author_email = ["xim@umich.edu", "ybchen@umich.edu"],
+    description = "A toolkit for creating N-dimensional (ND) dendrogram",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    install_requires = ["numpy>=1.18"],
+    python_requires = ">=3.8",
+)
