c538d0a1e54027b08ecb674434ffd88297eaffc0
Bill Chen <ybchen@umich.edu>
2021-07-17 22:25:01 +0800

    Updated __init__

--- dendrogram/__init__.py
+++ dendrogram/__init__.py
@@ -3,6 +3,6 @@ from .percolationND import *
 from .structureTree import *
 
 __all__ = percolationND.__all__ + structureTree.__all__
-__version__ = "0.0.dev"
+__version__ = "0.1"
 __name__ = "dendrogram"
 __author__ = ["Xi Meng", "Bill Chen"]
