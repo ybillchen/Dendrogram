62b6cdd7147451c2c3ae75e372c3fa3c854934fc
Bill Chen <ybchen@umich.edu>
2021-07-18 23:31:50 +0800

    Updated structureTree: updated docstrings

--- dendrogram/structureTree.py
+++ dendrogram/structureTree.py
@@ -1,4 +1,7 @@
 # Licensed under MIT License - see LICENSE
+"""
+A tree class and a makeTree function.
+"""
 
 import numpy as np
 from .percolationND import latticeND
@@ -6,32 +9,39 @@ from .percolationND import latticeND
 __all__ = ["clusterTree", "makeTree"]
 
 class clusterTree():
+    """
+    A tree class. 
+
+    `clusterTree` can be either the tree itself or the branch of a tree.
+    """
+
     def __init__(self, label, mask, isleaf=True):
+        """
+        Args:
+            label (:obj:`int`): Label of this tree/branch. 
+            mask (:obj:`numpy.ndarray` of :obj:`bool`): The area ocupied by
+                this tree/branch.
+            isleaf (:obj:`bool`, default to True): Whether this is a leaf 
+                branch (or "leaf" in brief) or not.
+        """
+
         self._label = label
-        self._branches = dict()
-        self._children = dict()
-        self._parent = dict()
         self._mask = mask.copy()
         self._isleaf = isleaf
 
+        self._branches = dict()
+        self._children = dict()
+        self._parent = dict()
+
     @property
     def label(self):
+        """:obj:`int`: Label of this tree/branch."""
         return self._label
 
     @property
-    def branches(self):
-        return self._branches
-    
-    @property
-    def children(self):
-        return self._children
-    
-    @property
-    def parent(self):
-        return self._parent
-    
-    @property
     def mask(self):
+        """:obj:`numpy.ndarray` of :obj:`bool`: Area ocupied by
+            this tree/branch."""
         return self._mask
 
     @mask.setter
@@ -40,28 +50,98 @@ class clusterTree():
 
     @property
     def isleaf(self):
+        """:obj:`bool`: Whether this is a leaf or not."""
         return self._isleaf
+
+    @property
+    def branches(self):
+        """:obj:`dict` of :obj:`int`: All branches of the tree. 
+            Empty if this is a branch."""
+        return self._branches
+    
+    @property
+    def children(self):
+        """:obj:`dict` of :obj:`int`: Children of this branch/tree. 
+            Empty if this is a leaf."""
+        return self._children
+    
+    @property
+    def parent(self):
+        """:obj:`dict` of :obj:`int`: Parent of this branch. 
+            Empty if this is a tree."""
+        return self._parent
     
     def create_leaf(self, label, mask):
+        """
+        Create a new leaf to the tree.
+
+        Note:
+            This method does *not* connect the new leaf to any of the 
+            presenting branch!
+
+        Args:
+            label (:obj:`int`): Label of the new leaf. 
+            mask (:obj:`numpy.ndarray` of :obj:`bool`): The area ocupied by
+                the new leaf.
+        """
+
         self._branches[label] = clusterTree(label, mask)
 
     def merge_branch(self, label, mask, branch):
+        """
+        Merge many old branches to a new branch. 
+
+        Note:
+            Here, "merge" means setting the new branch to be the parent of
+            old branches, *not* removing them!
+
+        Args:
+            label (:obj:`int`): Label of the new branch. 
+            mask (:obj:`numpy.ndarray` of :obj:`bool`): The area ocupied by
+                the new branch.
+            branch (:obj:`set` or :obj:`list` of :obj:`int`): Labels of 
+                old branches to be merged.
+        """
+
         self.branches[label] = clusterTree(label, mask, False)
         for i in branch: 
             self._branches[label]._children[i] = self._branches[i]
             self._branches[i]._parent[label] = self._branches[label]
 
-    def merge_final(self, branch):
+    def merge_final(self, branch):        
+        """
+        Merge final branches to the tree. 
+
+        Note:
+            Even if there is only one final branch, we still need to merge
+            (or, more properly, "link") it to the tree. Mind the difference 
+            between "tree" and "branch".
+
+        Args:
+            branch (:obj:`set` or :obj:`list` of :obj:`int`): Labels of 
+                final branches to be merged.
+        """
+
         self._isleaf = False
         for i in branch: 
             self._children[i] = self._branches[i]
             self._branches[i]._parent[-1] = self
             self._mask = self._mask | self._branches[i]._mask
 
-    def topology(self):
-        if len(self._branches) > 100:
-            print("Too large tree to be visualized.")
-            return ""
+    def topology(self, print_text=True):        
+        """
+        Print topology of the tree in pure text. 
+
+        Note:
+            Applying this method to large (with more than 100 branches)
+            or deep (with more than 100 levels) trees is not recommend.
+
+        Args:
+            print_text (:obj:`bool`): Whether to print to screen or not.
+
+        Returns:
+            :obj:`str`: Visualized topology of this tree.
+        """
 
         stack = [self]
         stage = [0]
@@ -81,8 +161,30 @@ class clusterTree():
 
 
 def makeTree(data, min_value, min_delta=0, min_npix=1, num_level=100):
-    max_level = np.nanmax(data)
-    level_list = np.linspace(max_level, min_value, num_level)
+    """
+    Make dendrogram tree from N-dimensional data.
+
+    Note:
+        Applying this method to large (with more than 100 branches)
+        or deep (with more than 100 levels) trees is not recommend.
+
+    Args:
+        data (:obj:`numpy.ndarray` of scalar): Data to make dendrogram tree.
+        min_value (scalar): Minimum value to consider.
+        min_delta (scalar, default to 0): Lag to be ignored. 
+        min_npix (:obj:`int`-like, default to 1): Minimum number of pixels 
+            to form a cluster.
+        num_level (:obj:`int`-like, default to 100): Number of levels.
+
+    Returns:
+        :obj:`clusterTree`: Tree for the dendrogram.
+
+    Examples:
+        >>> tree = makeTree(data, 0)
+    """
+
+    max_value = np.nanmax(data)
+    level_list = np.linspace(max_value, min_value, num_level)
     tree = clusterTree(-1, np.zeros(data.shape, dtype=bool))
     new_label = 0
     current_label = -1 * np.ones(data.shape, dtype=int)
