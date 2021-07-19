5d726aa0d5ea6a759488aa9ad554250156f14585
Bill Chen <ybchen@umich.edu>
2021-07-19 10:15:50 +0800

    Updated __init__

--- dendrogram/__init__.py
+++ dendrogram/__init__.py
@@ -1,10 +1,10 @@
 # Licensed under MIT License - see LICENSE
 
-from . import percolationND, structureTree
-from .percolationND import *
+from . import latticeND, structureTree
+from .latticeND import *
 from .structureTree import *
 
-__all__ = percolationND.__all__ + structureTree.__all__
+__all__ = latticeND.__all__ + structureTree.__all__
 __version__ = "0.1.dev"
 __name__ = "dendrogram"
 __author__ = ["Xi Meng", "Bill Chen"]
