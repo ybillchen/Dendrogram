6e7155aaf361cf55ff2abd3220fbb2d4f2484086
Bill Chen <ybchen@umich.edu>
2021-07-19 14:55:43 +0800

    Updated conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,7 +33,7 @@ release = '0.2.dev'
 extensions = [
 	'sphinx.ext.autodoc',
 	'sphinx.ext.napoleon',
-	'myst_parser'
+	# 'myst_parser'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
