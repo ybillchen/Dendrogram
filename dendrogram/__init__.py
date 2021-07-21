0638bfe6fa2008872d9d9cfe714ef5d7302b0a2e
Bill Chen <ybchen@umich.edu>
2021-07-22 00:38:49 +0800

    Updated version

--- dendrogram/__init__.py
+++ dendrogram/__init__.py
@@ -5,6 +5,6 @@ from .lattice import *
 from .structureTree import *
 
 __all__ = lattice.__all__ + structureTree.__all__
-__version__ = "0.3.2"
+__version__ = "0.3.dev"
 __name__ = "dendrogram"
 __author__ = ["Xi Meng", "Bill Chen"]
